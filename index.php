8b1ee276ad7c23f1b59e5e2ccfe0cb7c6c0a5ebb
chaitu111 <koripellachaitanya11@gmail.com>
2018-09-08 23:45:07 +0530

    hacking

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
-<?php 
-echo  "==========="
-?>
+Thank you for teaching me Git
+
+--->git init  ----Initializing the git repo
+---->git clone githiburl
+--->git add filename
+    git commit -m "test msg"
+    git push --- to upload to repository
+--->git pull ---- to get files from git repository
+--->git rm filename ---- to remove file
+    git commit -m "msg"
